a2253328671b91caf5098548cc19466d11ac2e7f
Ricardo J González C <rjgcastillo@hotmail.com>
2021-03-24 15:10:16 -0600

    Modify configuration for sweep of only rexnet_200

--- assignment/scripts/trainer.py
+++ assignment/scripts/trainer.py
@@ -57,9 +57,7 @@ class Trainer():
 
     def train_and_test(self, model: PretrainedModel, epochs: int, datamodule):
         last_trainer = self._fit_cycle(model, epochs, datamodule)
-        best_path = last_trainer.checkpoint_callback.best_model_path
-        model.load_from_checkpoint(best_path)
-        last_trainer.test(model, datamodule=datamodule, ckpt_path=None)
+        last_trainer.test(model, datamodule=datamodule)
 
     @staticmethod
     def add_argparse_args(parent_parser):
